0a8e57ac452ec0f2cdf90df5ac4fe6f965f2875a
Harman <harman5@sheridancollege.ca>
2025-07-02 10:08:46 -0400

    Update auth.routes.js
    Comment out - router.post---logout

--- server/routes/auth.routes.js
+++ server/routes/auth.routes.js
@@ -4,6 +4,6 @@ const authController = require('../controllers/auth.controller');
 
 router.post('/register', authController.register);
 router.post('/login', authController.login);
-router.post('/logout', authController.logout); // 👈 each post or get should be before export
+// router.post('/logout', authController.logout); 
 
 module.exports = router;

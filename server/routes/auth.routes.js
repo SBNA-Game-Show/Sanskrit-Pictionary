3d68f8efec4bdcd677d92ee7b2ab79bd7dba41d0
UsmanKhanDeveloper <khan356@sheridancollege.ca>
2025-07-04 14:07:21 -0400

    commented out router.post(logout) due to undefined/not created logout function. and changed index.html title from react app to sanskrit pictionary

--- server/routes/auth.routes.js
+++ server/routes/auth.routes.js
@@ -1,9 +1,9 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const authController = require('../controllers/auth.controller');
+const authController = require("../controllers/auth.controller");
 
-router.post('/register', authController.register);
-router.post('/login', authController.login);
-router.post('/logout', authController.logout); // 👈 each post or get should be before export
+router.post("/register", authController.register);
+router.post("/login", authController.login);
+//router.post('/logout', authController.logout); // 👈 each post or get should be before export
 
 module.exports = router;
